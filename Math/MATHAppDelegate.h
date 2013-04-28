fb99a7fd62c203abe5090ceb2db82698eea403f2
Jason Brennan <i.jasonbrennan@gmail.com>
2013-04-28 15:49:19 -0400

    Initial Commit

---
+++ Math/MATHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MATHAppDelegate.h
+//  Math
+//
+//  Created by Jason Brennan on Apr-28-2013.
+//  Copyright (c) 2013 Jason Brennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MATHAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Math/MATHAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MATHAppDelegate.m
+//  Math
+//
+//  Created by Jason Brennan on Apr-28-2013.
+//  Copyright (c) 2013 Jason Brennan. All rights reserved.
+//
+
+#import "MATHAppDelegate.h"
+
+@implementation MATHAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
