8b01c4de76e5bcf419d401da73dee0c1b5d1e12f
Jason Brennan <i.jasonbrennan@gmail.com>
2013-07-25 20:41:06 -0400

    Added the Math knob.

---
+++ Math/Views/MATHKnob.h
@@ -0,0 +1,13 @@
+//
+//  MATHKnob.h
+//  Math
+//
+//  Created by Jason Brennan on Jul-25-2013.
+//  Copyright (c) 2013 Jason Brennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MATHKnob : NSView
+
+@end
---
+++ Math/Views/MATHKnob.m
@@ -0,0 +1,26 @@
+//
+//  MATHKnob.m
+//  Math
+//
+//  Created by Jason Brennan on Jul-25-2013.
+//  Copyright (c) 2013 Jason Brennan. All rights reserved.
+//
+
+#import "MATHKnob.h"
+
+@implementation MATHKnob
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+	
+}
+
+@end
